0c4aa90d313ae26e2b0333f404a42ea7066ad3d6
Pavel Matushkin <pmatushkin@gmail.com>
2021-01-09 22:44:23 -0800

    fix: Remove dummy data

--- starter/app/src/main/java/com/udacity/shoestore/viewmodels/MainActivityViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/viewmodels/MainActivityViewModel.kt
@@ -14,32 +14,6 @@ class MainActivityViewModel : ViewModel() {
         // Build a sample list of shoes
         val shoes: ArrayList<Shoe> = ArrayList()
 
-        // Attribution note: name, company, and description strings below are copied from Zappos web site
-        shoes.add(
-            Shoe(
-                "Air Zoom Pegasus 37",
-                10.0,
-                "Nike",
-                "From the indoor track to the treadmill, these running shoes will take your running routine further than ever before"
-            )
-        )
-        shoes.add(
-            Shoe(
-                "Fresh Foam Tempo",
-                11.0,
-                "New Balance",
-                "These running shoes offer a colorful and supportive ride for your runs around the track"
-            )
-        )
-        shoes.add(
-            Shoe(
-                "GT-2000 8",
-                10.5,
-                "Asics",
-                "Get ready to run your best run yet with these running shoes"
-            )
-        )
-
         _shoeList.value = shoes
     }
 }
